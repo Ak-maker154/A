65672bd84c536a228a4b2096815bc4b267352199
Ak-maker154 <asarmy747533@gmail.com>
2026-08-05 15:57:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string, request, redirect
+from flask import Flask, render_template_string, request
 import requests
 import os
 
@@ -71,5 +71,6 @@ def index():
     return render_template_string(HTML_TEMPLATE)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     
